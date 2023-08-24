aa725206677c235c7e7d65bcfce60b87cac19dee
Lottipopps <122055716+Lottipopps@users.noreply.github.com>
2023-08-24 18:00:03 +0200

    Initial layout for Projects.js

--- src/Projects.js
+++ src/Projects.js
@@ -2,5 +2,18 @@ import React from "react";
 import "./Projects.css";
 
 export default function Projects() {
-  return <div className="Projects">This is the Projects Gallery</div>;
+  return (
+    <div className="Projects">
+      <div className="container">
+        <div className="row">
+          <h3>Project gallery</h3>
+        </div>
+        <div className="row">
+          <div className="col-md-4">Project 1</div>
+          <div className="col-md-4">Project 2</div>
+          <div className="col-md-4">Project 3</div>
+        </div>
+      </div>
+    </div>
+  );
 }

60358a9e4b380166df809876562c90edc27c6ace
Lottipopps <122055716+Lottipopps@users.noreply.github.com>
2023-08-24 16:27:23 +0200

    Created Navbar content

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,6 +1,35 @@
 import React from "react";
 import "./Navbar.css";
+import LogoDay from "./images/Opera-Code-Logo-Day.png";
+import LogoNight from "./images/Opera-Code-Logo-Night.png";
 
 export default function Navbar() {
-  return <div className="Navbar">This is the Navbar</div>;
+  return (
+    <div className="Navbar sticky-top">
+      <ul className="d-flex justify-content-between">
+        <li>
+          <a href="/">
+            <img src={LogoDay} alt="Logo" id="logo" />
+          </a>
+        </li>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/#Projects">Projects</a>
+        </li>
+        <li>
+          <a href="/#CV">CV</a>
+        </li>
+        <li>
+          <a href="/#Contacts">Contacts</a>
+        </li>
+        <li>
+          <i class="fa-solid fa-sun" style={{ color: "#34495e" }}></i>
+          {"   "}
+          <i class="fa-solid fa-moon" style={{ color: "#34495e" }}></i>
+        </li>
+      </ul>
+    </div>
+  );
 }

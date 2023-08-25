2862ef16d70990df25fdb39e8ab9819cde0a5b69
Lottipopps <122055716+Lottipopps@users.noreply.github.com>
2023-08-25 14:19:49 +0200

    Removed unnecessary vars

--- src/Projects.js
+++ src/Projects.js
@@ -2,10 +2,7 @@ import React from "react";
 import "./Projects.css";
 import Cards from "./Cards";
 import "bootstrap/dist/css/bootstrap.min.css";
-import DictionaryApp from "./images/Dictionary-App.png";
-import CharlieTheHomebaker from "./images/Charlie-The-Homebaker.png";
-import WorldClock from "./images/World-Clock.png";
-import ReactWeatherApp from "./images/React-Weather-App.png";
+
 
 export default function Projects() {
   return (

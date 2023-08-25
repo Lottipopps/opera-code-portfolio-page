06868ed0a7a0449dce1a68ebca8ba2661fdb8956
Lottipopps <122055716+Lottipopps@users.noreply.github.com>
2023-08-25 16:21:01 +0200

    Set up structure of footer

--- src/Footer.js
+++ src/Footer.js
@@ -2,5 +2,67 @@ import React from "react";
 import "./Footer.css";
 
 export default function Footer() {
-  return <div className="Footer">This is the footer of the page</div>;
+  return (
+    <footer className="Footer">
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-6 first">
+            <h6>Useful links to learn how to code 🫶</h6>
+            <ul>
+              <li>
+                <a
+                  href="https://www.shecodes.io/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  SheCodes
+                </a>{" "}
+                by Matt Delac and his team, excellent for beginners!
+              </li>
+              <li>
+                <a
+                  href="https://www.khanacademy.org/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Khan Academy
+                </a>{" "}
+                not just for coding, and completely free!
+              </li>
+              <li>
+                <a href="https://www.codecademy.com/">Codecademy</a> good for
+                everything Tech-related and available in monthly plans!
+              </li>
+              <li>
+                <a href="https://www.freecodecamp.org/learn/">FreeCodeCamp</a>,
+                just as it says on the label, free and very complete, just not
+                very beginner friendly.
+              </li>
+            </ul>
+          </div>
+          <div className="col-sm-6 second">
+            <h6>Contact me</h6>
+            <ul>
+              <li>
+                <a href="">
+                  <i className="fa-brands fa-github"></i>
+                </a>
+              </li>
+              <li>
+                <a href="">
+                  <i className="fa-brands fa-linkedin"></i>
+                </a>
+              </li>
+              <li>
+                <a href="">
+                  <i class="fa-solid fa-envelope"></i>
+                </a>
+              </li>
+            </ul>
+            <p>lotpop.chocolateuk@gmail.com</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }

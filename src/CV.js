c0ebe0f69c8b7ec93f92d6e01b7169454a996b65
Lottipopps <122055716+Lottipopps@users.noreply.github.com>
2023-08-25 15:07:25 +0200

    Added CV

--- src/CV.js
+++ src/CV.js
@@ -1,6 +1,12 @@
 import React from "react";
-import "./CV.css"
+import "./CV.css";
+import DevResume from "./Carlotta_Missiroli___Dev_Resume.pdf";
 
-export default function CV (){
-    return <div className="CV">This is my CV</div>
+export default function CV() {
+  return (
+    <div className="CV">
+      <h4>Just click below to know my journey</h4>
+      <a href={DevResume} target="_blank" rel="noreferrer">CV</a>
+    </div>
+  );
 }

54c447d458144626c4aa1635e96cc5dd7525af33
Lottipopps <122055716+Lottipopps@users.noreply.github.com>
2023-08-26 16:40:33 +0200

    Set up Dark/Light mode toggle

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
+import { React, useState } from "react";
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Link } from "react-router-dom";
 import Navbar from "./Navbar";
 import OperaHero from "./OperaHero";
 import Projects from "./Projects";
 import CV from "./CV";
 import Contacts from "./Contacts";
 import Footer from "./Footer";
+import LogoDay from "./images/Opera-Code-Logo-Day.png";
+import LogoNight from "./images/Opera-Code-Logo-Night.png";
 
 function App() {
+  let [yesDark, setYesDark] = useState(false);
+  function setMode() {
+    setYesDark(!yesDark);
+  }
   return (
     <div className="App">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<OperaHero />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/cv" element={<CV />} />
-        <Route path="/contacts" element={<Contacts />} />
-      </Routes>
-      <Footer />
+      <div className={yesDark ? "dark-mode" : "light-mode"}>
+        <nav className="Navbar sticky-top">
+          <ul className="d-flex justify-content-between">
+            <li>
+              <Link to="/">
+                {yesDark ? (
+                  <img src={LogoNight} alt="Logo-Night" />
+                ) : (
+                  <img src={LogoDay} alt="Logo-Day" />
+                )}
+              </Link>
+            </li>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/projects">Projects</Link>
+            </li>
+            <li>
+              <Link to="/cv">CV</Link>
+            </li>
+            <li>
+              <Link to="/contacts">Contacts</Link>
+            </li>
+            <li onClick={setMode}>
+              {yesDark ? (
+                <i className="fa-solid fa-sun" style={{ color: "#62929a" }}></i>
+              ) : (
+                <i
+                  className="fa-solid fa-moon"
+                  style={{ color: "#34495e" }}
+                ></i>
+              )}
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<OperaHero />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/cv" element={<CV />} />
+          <Route path="/contacts" element={<Contacts />} />
+        </Routes>
+        <Footer />
+      </div>
     </div>
   );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,9 +1,15 @@
-import React from "react";
+import { React, useState } from "react";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
 import LogoDay from "./images/Opera-Code-Logo-Day.png";
+import LogoNight from "./images/Opera-Code-Logo-Night.png";
 
 export default function Navbar() {
+  let [yesDark, setYesDark] = useState(false);
+  function setMode() {
+    setYesDark(!yesDark);
+  }
+
   return (
     <nav className="Navbar sticky-top">
       <ul className="d-flex justify-content-between">
@@ -24,7 +30,7 @@ export default function Navbar() {
         <li>
           <Link to="/contacts">Contacts</Link>
         </li>
-        <li>
+        <li onClick={setMode}>
           <i className="fa-solid fa-sun" style={{ color: "#34495e" }}></i>
           {"   "}
           <i className="fa-solid fa-moon" style={{ color: "#34495e" }}></i>
